d8f6e70a2f1da6a8566dd461ee4a4f97727aa8bf
paul-zz <593958169@qq.com>
2022-12-19 21:14:43 +0100

    bug fix in checkForTwo

--- ExerciseNine/src/Game/Chessboard.java
+++ ExerciseNine/src/Game/Chessboard.java
@@ -139,15 +139,15 @@ public class Chessboard {
 		// Check for rows
 		for(int i=0;i<3;i++)
 		{
-			if(board[i][0]==chess_int && board[i][0]==board[i][1])
+			if(board[i][0]==chess_int && board[i][0]==board[i][1] && board[i][2]==0)
 			{
 				return locationToPlace(i, 2);
 			}
-			if(board[i][0]==chess_int && board[i][0]==board[i][2])
+			if(board[i][0]==chess_int && board[i][0]==board[i][2] && board[i][1]==0)
 			{
 				return locationToPlace(i, 1);
 			}
-			if(board[i][1]==chess_int && board[i][1]==board[i][2])
+			if(board[i][1]==chess_int && board[i][1]==board[i][2] && board[i][0]==0)
 			{
 				return locationToPlace(i, 0);
 			}
@@ -155,41 +155,41 @@ public class Chessboard {
 		// Check for cols
 		for(int i=0;i<3;i++)
 		{
-			if(board[0][i]==chess_int && board[0][i]==board[1][i])
+			if(board[0][i]==chess_int && board[0][i]==board[1][i] && board[2][i]==0)
 			{
 				return locationToPlace(2, i);
 			}
-			if(board[0][i]==chess_int && board[0][i]==board[2][i])
+			if(board[0][i]==chess_int && board[0][i]==board[2][i] && board[1][i]==0)
 			{
 				return locationToPlace(1, i);
 			}
-			if(board[1][i]==chess_int && board[1][i]==board[2][i])
+			if(board[1][i]==chess_int && board[1][i]==board[2][i] && board[0][i]==0)
 			{
 				return locationToPlace(0, i);
 			}
 		}
 		// Check for two diagonals
-		if(board[0][0]==chess_int && board[0][0]==board[1][1])
+		if(board[0][0]==chess_int && board[0][0]==board[1][1] && board[2][2]==0)
 		{
 			return locationToPlace(2, 2);
 		}
-		if(board[0][0]==chess_int && board[0][0]==board[2][2])
+		if(board[0][0]==chess_int && board[0][0]==board[2][2] && board[1][1]==0)
 		{
 			return locationToPlace(1, 1);
 		}
-		if(board[1][1]==chess_int && board[2][2]==board[1][1])
+		if(board[1][1]==chess_int && board[2][2]==board[1][1] && board[0][0]==0)
 		{
 			return locationToPlace(0, 0);
 		}
-		if(board[0][2]==chess_int && board[0][2]==board[1][1])
+		if(board[2][0]==chess_int && board[2][0]==board[1][1] && board[0][2]==0)
 		{
-			return locationToPlace(2, 0);
+			return locationToPlace(0, 2);
 		}
-		if(board[0][2]==chess_int && board[0][2]==board[2][0])
+		if(board[0][2]==chess_int && board[0][2]==board[2][0] && board[1][1]==0)
 		{
 			return locationToPlace(1, 1);
 		}
-		if(board[1][1]==chess_int && board[0][2]==board[1][1])
+		if(board[1][1]==chess_int && board[0][2]==board[1][1] && board[2][0]==0)
 		{
 			return locationToPlace(2, 0);
 		}
